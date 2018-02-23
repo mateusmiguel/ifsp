e16dc9edcee9451acf03a77a2918f75a898e4ebf
Mateus Miguel <webmateusmiguel@gmail.com>
2018-02-23 17:11:53 -0300

    exercises

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-    printf("Hello Mateus\n");
-    return 0;
+   float a, b, resDivisao;
+   a = 11;
+   printf("\nDigite um numero para dividirmos por 11: ");
+   scanf("%f", &b);
+   
+   resDivisao = a/b;
+   
+   printf("\nO resultado dessa divisao eh: %f \n", resDivisao);
+   system("PAUSE");
+   return 0;
 }
---
+++ s1e1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+    i = 123;
+    printf("%d\n", i);
+    system("PAUSE");
+    return 0;
+}
