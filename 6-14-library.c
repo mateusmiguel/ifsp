857108cc6296cbca6932c6cc8e98240d2526fe5e
Mateus Miguel <webmateusmiguel@gmail.com>
2018-03-19 10:18:03 -0300

    Exercises

--- 6-14-library.c
+++ 6-14-library.c
@@ -2,48 +2,27 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-//	char tipo_usuario, classif_livro; 
-	
-//	printf("Informe o tipo de usuário e a classificação do livro desejado: "); 
-//   	scanf("%c %c", &tipo_usuario, &classif_livro); 
-//   
-//   	if ((tipo_usuario == 'P') || (tipo_usuario == 'A' && classif_livro == 'A'))
-//		printf("Pode locar o livro\n"); 
-//   	else 
-//    	printf("Não pode locar o livro\n"); 
-   
+  
 	char userType, bookCategory;
-
-	printf("User type: ");
-	getchar();
+	
+	printf("User type ('T' for teacher, 'S' for student): ");
 	scanf("%c", &userType);
-	
-	printf("Book category: ");
 	getchar();
-	scanf("%c", &bookCategory);
-	
-		switch (category) {
-		case 'a' :
-			
-	    	break;
-	    	
-		case 'b' :
-	    	
-	    	break;
-		
-		case 'c' :
-			
-	    	break;
-	     
-	    default :
-	    	printf ("Category invalid. Please, try again.");
-			exit(0);
-	}
-		
-	if((userType != 'T') && (bookCategory == 'B'))  {
-		printf("Denny. \n");
+	if (userType !='T' && userType!='S') {
+		printf("Please, digit a valid user type ('T' for teacher, 'S' for student)");
 	} else {
-		printf("Alow.\n");
+		printf("Book category ('A' or 'B'): ");
+		scanf("%c", &bookCategory);
+		getchar();
+		if ((bookCategory != 'A' && bookCategory != 'B')) {
+			printf("Please, digit a valid book category ('A' or 'B')");
+		} else {
+			if ((userType == 'T') || (userType == 'S' && bookCategory == 'A')) {
+				printf("Allow. You can lease the book.\n");
+			} else {
+				printf("Denny. You can't lease the book.");
+			}
+		}
 	}
 
 	return 0;
